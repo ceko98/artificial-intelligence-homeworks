496b55b9c875106dc8666d2fab1e68dde4655769
ceko98 <ceko9898@gmail.com>
2020-11-15 23:29:38 +0200

    Add another random gen

--- 8-queens/8-queens.cpp
+++ 8-queens/8-queens.cpp
@@ -2,18 +2,21 @@
 #include <time.h>
 #include <iostream>
 #include <cmath>
+#include <random>
 
 class Board
 {
 private:
     int size;
-    int *queens;
-    int *row;
-    int *diag1;
-    int *diag2;
+    int *queens = nullptr;
+    int *row = nullptr;
+    int *diag1 = nullptr;
+    int *diag2 = nullptr;
 
-    int *tmp;
+    int *tmp = nullptr;
     int tmpIndex;
+
+    std::mt19937 rng;
 
 public:
     Board(int size);
@@ -30,37 +33,13 @@ public:
     void solve();
     void init();
     void clean();
-
-    void printArrays()
-    {
-
-        for (int i = 0; i < this->size; i++)
-        {
-            std::cout << queens[i] << " ";
-        }
-        std::cout << std::endl;
-
-        for (int i = 0; i < this->size; i++)
-        {
-            std::cout << row[i] << " ";
-        }
-        std::cout << std::endl;
-
-        for (int i = 0; i < this->size * 2 - 1; i++)
-        {
-            std::cout << diag1[i] << " ";
-        }
-        std::cout << std::endl;
-
-        for (int i = 0; i < this->size * 2 - 1; i++)
-        {
-            std::cout << diag2[i] << " ";
-        }
-        std::cout << std::endl;
-    }
 };
 
-Board::Board(int size) : size(size)
+Board::Board(int size) : size(size), rng(std::random_device{}())
+{
+}
+
+void Board::init()
 {
     this->queens = new int[this->size]();
     this->row = new int[this->size]();
@@ -78,6 +57,15 @@ Board::Board(int size) : size(size)
         this->diag1[i - queens[i] + this->size - 1]++;
         this->diag2[i + queens[i]]++;
     }
+}
+
+void Board::clean()
+{
+    delete[] queens;
+    delete[] row;
+    delete[] diag1;
+    delete[] diag2;
+    delete[] tmp;
 }
 
 int Board::findMinConflict(int column)
@@ -108,7 +96,8 @@ int Board::conflictsFor(int column, int row)
 
 int Board::getRamdomFromTmp()
 {
-    return tmp[rand() % tmpIndex];
+    std::uniform_int_distribution<int> dist(0, tmpIndex - 1);
+    return tmp[dist(rng)];
 }
 
 void Board::printBoard()
@@ -163,7 +152,7 @@ int Board::getMinQueenConflicts(int column)
 
     for (int i = 0; i < this->size; i++)
     {
-        int conflicts = this->conflictsFor(column, i) - (i == queens[column] ? 3 : 0);
+        int conflicts = this->conflictsFor(column, i);
         if (conflicts < minConflicts)
         {
             tmpIndex = 0;
@@ -191,10 +180,22 @@ void Board::moveQueen(int column, int newRow, int oldRow)
 
 void Board::solve()
 {
+    clock_t tStart = clock();
+    init();
+
     int index = 0;
     while (index++ <= 3 * this->size)
     {
         int column = getMaxConflictQueen();
+        if (conflictsFor(column, queens[column]) - 3 == 0)
+        {
+            if (size < 10)
+            {
+                printBoard();
+            }
+            printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+            return;
+        }
         int oldRow = queens[column];
         int newRow = getMinQueenConflicts(column);
         moveQueen(column, newRow, oldRow);
@@ -202,27 +203,22 @@ void Board::solve()
 
     if (hasConflicts())
     {
-        std::cout << "sad" << std::endl;
-        return;
+        clean();
+        solve();
     }
-    printBoard();
-    std::cout << "heppy" << std::endl;
+    printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 }
 
 Board::~Board()
 {
-    delete[] queens;
-    delete[] row;
-    delete[] diag1;
-    delete[] diag2;
-    delete[] tmp;
+    clean();
 }
 
-int main ()
+int main()
 {
     int n;
     std::cin >> n;
-    
+
     Board s(n);
     s.solve();
 
